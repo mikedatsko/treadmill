9eea0e3d1440fac8e74a29a0b907775c2df7023d
Mykhailo Datsko <mike@datsko.net>
2019-02-15 18:35:15 +0200

    match fix

--- src/components/EntertainmentView.tsx
+++ src/components/EntertainmentView.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import videos from '../mocks/videos';
 
-const EntertainmentView = ({ match }) => (
+const EntertainmentView = (props: any) => (
   <div>
     <h1>Entertainment View</h1>
 
     <iframe
-      src={videos[match.params.id]}
+      src={videos[props.match.params.id]}
       width={'988px'}
       height={'530px'}
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
